b0e2efa11e45dec5a574819c21521147c270103c
Bloodcount <searosss@gmail.com>
2016-05-28 02:37:33 +0300

    json api bindings. (todo: jsonrpc)

--- request_api/api/main.go
+++ request_api/api/main.go
@@ -31,6 +31,180 @@ func main() {
 }
 
 func JSONHandler(w http.ResponseWriter, r *http.Request) {
+w.Header().Set("Access-Control-Allow-Origin", "*")
+    if r.Method != "POST" {
+        WriteResp(w, http.StatusMethodNotAllowed,
+            `{"jsonrpc": "2.0", "error": {"code": -32700, "message": "Parse error!"}, "id": "1"}`)
+        glog.Error(r.Method)
+        return
+    }
+
+    bytes, err := ioutil.ReadAll(r.Body)
+    if err != nil {
+        WriteResp(w, http.StatusInternalServerError,
+            `{"jsonrpc": "2.0", "error": {"code": -32700, "message": "Parse error!"}, "id": "1"}`)
+        return
+    }
+
+    var apiReq JsonRPCReq
+    err = json.Unmarshal(bytes, &apiReq)
+    if err != nil {
+        glog.Error("Json parse error: ", err)
+        WriteResp(w, http.StatusInternalServerError,
+            `{"jsonrpc": "2.0", "error": {"code": -32700, "message": "Parse error!"}, "id": "1"}`)
+        return
+    }
+
+    glog.Info(`!!!!!!!!apiRequest: `, apiReq)
+    if apiReq.Method == `GetTableDetails` {
+        table, err := GetPlaceAndTable(apiReq.Params["tableID"])
+        if err != nil {
+            glog.Info("JSON marshal error: ", err.Error())
+            WriteResp(w, http.StatusInternalServerError,
+                `{"jsonrpc": "2.0", "error": {"code": -32603, "message": "Internal error!"}, "id": "1"}`)
+            return
+        }
+        bytes, err = json.Marshal(table)
+        if err != nil {
+            glog.Info("JSON marshal error: ", err.Error())
+            WriteResp(w, http.StatusInternalServerError,
+                `{"jsonrpc": "2.0", "error": {"code": -32603, "message": "Internal error!"}, "id": "1"}`)
+            return
+        }
+    } else if apiReq.Method == `GetPendingRequests` {
+
+        placeID, err := strconv.Atoi(apiReq.Params["placeID"])
+        if err != nil {
+            glog.Info("placeID is not an integer: ", apiReq.Params["placeID"])
+            WriteResp(w, http.StatusInternalServerError,
+                `{"jsonrpc": "2.0", "error": {"code": -32602, "message": "Invalid method parameter(s)."}, "id": "1"}`)
+            return
+        }
+        requests, err := GetPendingRequests(placeID)
+
+        if err != nil && err.Error() == `missing_row` {
+            glog.Info("GetPendingRequests error: ", err.Error())
+            WriteResp(w, http.StatusInternalServerError,
+                `{"jsonrpc": "2.0", "error": {"code": -32603, "message": "Non existant row!"}, "id": "1"}`)
+            return
+        }
+
+        if err != nil {
+            glog.Info("GetPendingRequests error: ", err.Error())
+            WriteResp(w, http.StatusInternalServerError,
+                `{"jsonrpc": "2.0", "error": {"code": -32603, "message": "Internal error!"}, "id": "1"}`)
+            return
+        }
+
+        bytes, err = json.Marshal(requests)
+        if err != nil {
+            glog.Info("JSON marshal error: ", err.Error())
+            WriteResp(w, http.StatusInternalServerError,
+                `{"jsonrpc": "2.0", "error": {"code": -32603, "message": "Internal error!"}, "id": "1"}`)
+            return
+        }
+    } else if apiReq.Method == `GetRequest` {
+        requestID, err := strconv.Atoi(apiReq.Params["requestID"])
+        if err != nil {
+            glog.Info("placeID is not an integer: ", apiReq.Params["requestID"])
+            WriteResp(w, http.StatusInternalServerError,
+                `{"jsonrpc": "2.0", "error": {"code": -32602, "message": "Invalid method parameter(s)."}, "id": "1"}`)
+            return
+        }
+        request, err := GetRequest(requestID)
+        if err != nil && err.Error() == `missing_row` {
+            glog.Info("GetPendingRequests error: ", err.Error())
+            WriteResp(w, http.StatusInternalServerError,
+                `{"jsonrpc": "2.0", "error": {"code": -32603, "message": "Non existant row!"}, "id": "1"}`)
+            return
+        }
+
+        if err != nil {
+            glog.Info("Error: ", err.Error())
+            WriteResp(w, http.StatusInternalServerError,
+                `{"jsonrpc": "2.0", "error": {"code": -32603, "message": "Internal error!"}, "id": "1"}`)
+            return
+        }
+
+        bytes, err = json.Marshal(request)
+        if err != nil {
+            glog.Info("JSON marshal error: ", err.Error())
+            WriteResp(w, http.StatusInternalServerError,
+                `{"jsonrpc": "2.0", "error": {"code": -32603, "message": "Internal error!"}, "id": "1"}`)
+            return
+        }
+    } else if apiReq.Method == `AddRequest` {
+        tableID, err := strconv.Atoi(apiReq.Params["tableID"])
+        if err != nil {
+            glog.Info("tableID is not an integer: ", apiReq.Params["tableID"])
+            WriteResp(w, http.StatusInternalServerError,
+                `{"jsonrpc": "2.0", "error": {"code": -32602, "message": "Invalid method parameter(s)."}, "id": "1"}`)
+            return
+        }
+
+        menuItemID, err := strconv.Atoi(apiReq.Params["menuItemID"])
+        if err != nil {
+            glog.Info("menuItemID is not an integer: ", apiReq.Params["menuItemID"])
+            WriteResp(w, http.StatusInternalServerError,
+                `{"jsonrpc": "2.0", "error": {"code": -32602, "message": "Invalid method parameter(s)."}, "id": "1"}`)
+            return
+        }
+
+        placeID, err := strconv.Atoi(apiReq.Params["placeID"])
+        if err != nil {
+            glog.Info("placeID is not an integer: ", apiReq.Params["placeID"])
+            WriteResp(w, http.StatusInternalServerError,
+                `{"jsonrpc": "2.0", "error": {"code": -32602, "message": "Invalid method parameter(s)."}, "id": "1"}`)
+            return
+        }
+
+        request, err := AddRequest(tableID, menuItemID, placeID)
+
+        if err != nil {
+            glog.Info("AddRequest error: ", err.Error())
+            WriteResp(w, http.StatusInternalServerError,
+                `{"jsonrpc": "2.0", "error": {"code": -32603, "message": "Internal error!"}, "id": "1"}`)
+            return
+        }
+
+        bytes, err = json.Marshal(request)
+        if err != nil {
+            glog.Info("JSON marshal error: ", err.Error())
+            WriteResp(w, http.StatusInternalServerError,
+                `{"jsonrpc": "2.0", "error": {"code": -32603, "message": "Internal error!"}, "id": "1"}`)
+            return
+        }
+
+    } else if apiReq.Method == `AckRequest` {
+        requestID, err := strconv.Atoi(apiReq.Params["requestID"])
+        if err != nil {
+            glog.Info("placeID is not an integer: ", apiReq.Params["requestID"])
+            WriteResp(w, http.StatusInternalServerError,
+                `{"jsonrpc": "2.0", "error": {"code": -32602, "message": "Invalid method parameter(s)."}, "id": "1"}`)
+            return
+        }
+        _, err = AckRequest(requestID)
+        if err != nil {
+            glog.Info("Error: ", err.Error())
+            WriteResp(w, http.StatusInternalServerError,
+                `{"jsonrpc": "2.0", "error": {"code": -32603, "message": "Internal error!"}, "id": "1"}`)
+            return
+        }
+
+        bytes, err = json.Marshal("asd")
+        if err != nil {
+            glog.Info("JSON marshal error: ", err.Error())
+            WriteResp(w, http.StatusInternalServerError,
+                `{"jsonrpc": "2.0", "error": {"code": -32603, "message": "Internal error!"}, "id": "1"}`)
+            return
+        }
+    } else {
+        WriteResp(w, http.StatusInternalServerError,
+            `{"jsonrpc": "2.0", "error": {"code": -32601, "message": "The method does not exist / is not available."}, "id": "1"}`)
+        return
+    }
+
+    WriteResp(w, http.StatusOK, string(bytes))
 }
 
 
